d7f5399f57fe8f6653ddec1f49ad1a266c6371ed
Documenter.jl <documenter@juliadocs.github.io>
2024-12-16 18:23:32 +0000

    build based on 9173360

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NumericalMethodsLib","category":"page"},{"location":"#NumericalMethodsLib","page":"Home","title":"NumericalMethodsLib","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NumericalMethodsLib.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NumericalMethodsLib]","category":"page"}]
+}
